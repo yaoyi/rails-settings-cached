d269f214cab207afd4dd1bf79f32e26cce409d0a
yaoyi <linyaoyi@me.com>
2019-06-26 22:40:59 +0800

    Remove cache

--- lib/rails-settings-cached.rb
+++ lib/rails-settings-cached.rb
@@ -3,7 +3,6 @@ require_relative "rails-settings/base"
 require_relative "rails-settings/scoped_settings"
 require_relative "rails-settings/default"
 require_relative "rails-settings/extend"
-require_relative "rails-settings/railtie"
 require_relative "rails-settings/version"
 
 module RailsSettings
